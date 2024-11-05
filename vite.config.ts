89612184561f3347a69c41600072d188faf67b5e
Usama Badar <103148408+usamabadarr@users.noreply.github.com>
2024-11-05 18:53:53 -0500

    Update vite.config.ts
    for render delpoyment purposes

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,11 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   envDir: './environment',
+//   plugins: [react()],
+// });
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -5,4 +13,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   envDir: './environment',
   plugins: [react()],
+  server: {
+    port: process.env.PORT ? Number(process.env.PORT) : 3000,  
+    host: '0.0.0.0',  
+  },
 });
