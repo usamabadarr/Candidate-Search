8ad1e387e02414c7257d10f9039fdddea25141f9
usamabadarr <usamabadarr@gmail.com>
2024-11-05 17:57:11 -0500

    added sist folder npm run build again

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import { Link } from 'react-router-dom';
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 
 const Home = () => {
   console.log('Home component rendered');
